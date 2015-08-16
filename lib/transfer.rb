7f228ac7c3995f6f007baa71cd529fd7022bf657
George Toothman <george.toothman@flatironschool.com>
2015-08-16 18:49:57 -0400

    transaction classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def both_valid?
+    if sender.valid? && receiver.valid?
+      true
+    end
+  end
+
+  def execute_transaction
+    if @amount > @sender.balance
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif @status != "complete"
+      @sender.balance = @sender.balance - amount
+      @receiver.balance = @receiver.balance + amount
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
 end
